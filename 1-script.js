1f1d9fe0821e11170161ea137d4ea10983b822e8
surendra <surendrapottipati5@gmail.com>
2025-05-16 15:13:53 +0530

    sample_code

---
+++ 1-script.js
@@ -0,0 +1,41 @@
+
+
+//console.log("hello world");
+//console.log(123);
+//console.log("surendra");
+
+document.write("Hello dear");
+document.write(123);
+document.write("Hlo world");
+document.write(143);
+window.alert("Hello world");
+
+
+/*var customerID = 40;
+console.log(customerID);*/
+ customerID=30;
+var customerID=40;
+console.log(customerID);
+console.log(customerID);
+
+var customerID = 30;    
+console.log(customerID);
+
+const num=[1,2,3];
+console.log(num);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
