5d5e2980b9298abb6c717225c7b3e7357d781970
surendra <surendrapottipati5@gmail.com>
2025-05-17 22:08:11 +0530

    variables

---
+++ Data-types/non-primitive.js
@@ -0,0 +1,12 @@
+//Arrays
+// number , string , boolean , null , undefined , array
+
+let ids = [1, 2, 3, 4, 5];
+let names = ["surendra", "suresh", "lucky"];
+let mixed = [3,"surendra",true, null, undefined];
+let MovieNames = ["Darling","salar","KGF"];
+
+console.log(ids);
+console.log(names);
+console.log(mixed); 
+console.log(MovieNames);
---
+++ variables/1-script.js
@@ -0,0 +1,104 @@
+/*CONSOLE*/
+
+//console.log("hello world");
+//console.log(123);
+//console.log("surendra");
+
+
+/*document.write("Hello dear");
+document.write(123);
+document.write("Hlo world");
+document.write(143);
+window.alert("Hello world");*/
+
+
+/*var customerID = 40;
+console.log(customerID);*/
+
+/*var customerID=30;
+var customerID=40;
+console.log(customerID);
+console.log(customerID);*/
+
+//var customerID = 30;    
+//console.log(customerID);
+
+//const num=[1,2,3];
+//console.log(num);
+
+/*VARIABLES*/
+
+let age = 20;
+let discount = 40;
+let customerName = "surendra";
+
+
+console.log(age);
+console.log(discount);
+console.log(customerName);
+
+
+
+/*STRING DATA TYPES*/
+
+//collection of characters a,b,c,$,{
+
+let name = "surendra";
+    instituteName = "surya software solutions";
+    addresh ="bangalore";
+
+console.log(name);
+console.log(instituteName);    
+console.log(addresh); 
+
+let name1 = "suresh";
+rollNumber = 123;   
+
+console.log(name1);
+console.log(rollNumber);
+
+/*boolean data types*/
+
+let single = true;
+console.log(single);
+
+let married = false;
+console.log(married);
+
+let goodboy = true; 
+let badboy = false;
+console.log(goodboy);
+console.log(badboy);
+
+
+/*undefined data types*/
+let joiningDate;
+console.log(joiningDate);
+
+/*defined data types*/
+let joiningDate1 = "05-06-2023";
+console.log(joiningDate1);
+
+/*null data types*/
+let customerName1 = null;
+console.log(customerName1);
+
+let customerName2 = "surendra";
+console.log(customerName2);
+
+/*symbol data types*/
+let symbol1 = Symbol("surendra");
+console.log(symbol1);//123
+
+let symbol2 = Symbol("123");
+console.log(symbol2);//surendra
+
+
+
+
+
+
+
+
+
+
