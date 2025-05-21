c2ec4c123a869717adb7cef643d6dca62ab2db9b
surendra <surendrapottipati5@gmail.com>
2025-05-21 21:14:55 +0530

    functins

--- Data-types/function.js
+++ Data-types/function.js
@@ -1,5 +1,5 @@
-/*//block1
- function showCustomerDetails(){
+//block1
+/* function showCustomerDetails(){
 let customerID = 123;
 console.log(customerID);
 
@@ -11,17 +11,52 @@ showCustomerDetails();
 
 //block2
 
-function showMovieDetails(){
+function showMovieDetails()
+{
 
 let movieNames = ["salaar","darling","kgf"];
-console.log(movieNames);
+console.log(movieNames[2]);
 
 let hitMovies = ["bahubali","shivaji","rajamouli"];
-console.log(hitMovies);
+console.log(hitMovies[0]);
 
 
-let moviyears = [2015,2016,2017];
-console.log(moviyears);  
+let movieyears = [2015,2016,2017];
+console.log(movieyears[1]);  
 
 }
-showCustomerDetails();*/
+showMovieDetails();*/
+
+//styles
+
+/*console.log("surendra")
+console.log("%csurendra","color:blue")
+console.log("%csurendra","background-color:red;color:black","font-size:20px")
+console.log("%c   ","background-color:blue")*/
+
+     function redcolor(){
+      console.log("%c    ","background-color:red");
+     }
+
+
+function firstsection(){
+    console.log("%c   ","background-color:orange");
+    console.log("%c   ","background-color:green");
+   redcolor();
+}
+firstsection();
+
+ 
+ function secondsection(){
+    console.log("%c   ","background-color:purple");
+    console.log("%c   ","background-color:black");
+   redcolor();
+}
+secondsection();
+
+ function Thirdsection(){
+    console.log("%c   ","background-color:blue");
+    console.log("%c   ","background-color:darkgrey");
+    redcolor();
+}
+Thirdsection();
