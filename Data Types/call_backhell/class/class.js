ae6fe72a0c7af250d796ce52d1a820033766c2d7
surendra <surendrapottipati5@gmail.com>
2025-06-09 22:59:30 +0530

    class

---
+++ Data Types/call_backhell/class/class.js
@@ -0,0 +1,19 @@
+//members -constructor -> properties ->Method =function
+class office{
+    
+
+    constructor(name){ //-->member
+        console.log(`emloyee name is ${name}` );
+
+    }
+    enrollemployee(){ //-->method
+        console.log("employee name is suri and he enroll id 123");
+    }
+
+}
+new office("suri").enrollemployee();
+//var mycompany = new office("suri");
+//mycompany.enrollemployee();
+
+
+
